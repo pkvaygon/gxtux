e20c4ea47243cd0047fd803461eeda07df279e38
Arishem <pkvaygon@gmail.com>
2023-04-25 19:49:57 +0600

    firstgit

---
+++ script.js
@@ -0,0 +1,71 @@
+// =======Объявление функции=========
+// function название функции (){}
+
+// function showMessage() {
+//     console.log('Hello');
+// }
+// showMessage()
+
+
+// function showString(message) {
+// console.log(message);
+// }
+// showString(['Hi World!', 'koko'])
+
+// function getUserName(userName) {
+// alert(userName)
+// }
+// getUserName('Nikita')
+// getUserName('m')
+
+// создать функцию getName
+// спросить у пользотвателя имя
+// и отобразить Приветсвую имя пользователя
+
+// function getName(name) {
+//     console.log(`Привет ${name}`);
+// }
+// getName(prompt('Ваше имя'))
+
+// function getResult(num1, num2) {
+// console.log(num1 + num2);
+// }
+// getResult(3, 5)
+// getResult(467, 5183)
+// getResult(30, 8)
+
+function calcNum(n1, n2) {
+    return n1 + n2
+}
+console.log(calcNum(1, 2));
+
+// спросить возраст если ему 18 и больше
+// тогда консоль проходите
+// иначе Доступ закрыт
+
+// function checkAge(age) {
+// age >= 18 ? console.log('проходите') : console.log('stop');
+// }
+// checkAge(prompt('ваш возраст'))
+// let koko = prompt('age')
+// function checkAge() {
+//     koko >= 18 ? console.log('проходите') : console.log('stop');
+// }
+// checkAge()
+
+//фунция invite
+// 1) Функция для приглашения гостей на мероприятие - invite
+// 2)спросить Количество гостей
+// 3) Запись имен
+// функция Present
+// 1) Вывод каждого приглашенного гостя
+// Пример: Уважаемый Олег, 
+// я приглашаю вас на празднование моего дня рождения!!!
+let guests = []
+
+function inviteGuest() {
+
+}
+
+
+
